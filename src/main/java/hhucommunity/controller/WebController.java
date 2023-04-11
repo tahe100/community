e0f4dd141e1758f48235f26d6e42cb954bcc9fd4
tehe100 <tahe100@uni-duesseldorf.de>
2023-04-11 18:14:22 +0800

    add bootstrap and index

--- src/main/java/hhucommunity/controller/WebController.java
+++ src/main/java/hhucommunity/controller/WebController.java
@@ -1,7 +1,12 @@
 package hhucommunity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
 }
